ecda41b1a7717a57c978636439e1f9336c404412
Anıl Soylu <56033298+anilsoylu@users.noreply.github.com>
2024-01-28 01:50:17 +0300

    feat: create user

--- actions/register.ts
+++ actions/register.ts
@@ -1,6 +1,9 @@
 "use server"
 import * as z from "zod"
+import bcrypt from "bcryptjs"
 import { RegisterSchema } from "@/schemas"
+import { db } from "@/lib/db"
+import { getUserByEmail } from "@/data/user"
 
 export const register = async (values: z.infer<typeof RegisterSchema>) => {
   const validatedFields = RegisterSchema.safeParse(values)
@@ -9,5 +12,24 @@ export const register = async (values: z.infer<typeof RegisterSchema>) => {
     return { error: "Invalid fields!" }
   }
 
-  return { success: "Email sent!" }
+  const { email, password, name } = validatedFields.data
+  const hashedPassword = await bcrypt.hash(password, 10)
+
+  const existingUser = await getUserByEmail(email)
+
+  if (existingUser) {
+    return { error: "Email already exists!" }
+  }
+
+  await db.user.create({
+    data: {
+      email,
+      password: hashedPassword,
+      name,
+    },
+  })
+
+  // TODO: Sen verification email
+
+  return { success: "User created!" }
 }
