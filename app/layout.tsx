219dca604c1014e51453b0dbb929a2f40f4e851a
Anıl Soylu <56033298+anilsoylu@users.noreply.github.com>
2024-01-28 06:53:49 +0300

    updated: font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,10 @@
 import type { Metadata } from "next"
-import { Inter as FontSans } from "next/font/google"
+import { Inter } from "next/font/google"
 import "./globals.css"
 
 import { cn } from "@/lib/utils"
 
-export const fontSans = FontSans({
+export const fontSans = Inter({
   subsets: ["latin"],
   variable: "--font-sans",
 })
